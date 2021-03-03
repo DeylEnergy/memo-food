dbcc85009c37b9929038c63c71c5a5443f2129f0
Deyl <soulvisman@gmail.com>
2021-03-03 19:00:19 +0600

    Add constants

---
+++ src/constants.ts
@@ -0,0 +1,43 @@
+export const BOARD_SIZE = 36;
+
+export const FOOD_PRESENCE_COUNT = 2;
+
+export const MATCH_PAIR_TIMEOUT_MS = 5000;
+
+export const FOOD = [
+  "apple",
+  "candy",
+  "cheese",
+  "cupcake",
+  "doughnut",
+  "egg",
+  "fries",
+  "gingerbread",
+  "hamburger",
+  "honey",
+  "hotdog",
+  "icecream",
+  "milk",
+  "orange",
+  "peach",
+  "pizza",
+  "pudding",
+  "watermelon",
+];
+
+export const SMALL_SCREEN_ICON_SIZE = 32;
+
+export const EXTRA_SMALL_SCREEN = {
+  ICON_SIZE: 32,
+  BOARD_PADDING_SPACING: 1,
+};
+
+export const SMALL_SCREEN = {
+  ICON_SIZE: 36,
+  BOARD_PADDING_SPACING: 1.5,
+};
+
+export const TABLET_SCREEN = {
+  ICON_SIZE: 48,
+  BOARD_PADDING_SPACING: 2,
+};
