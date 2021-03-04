5975d0c6613e1e77189753a72075858914891641
Deyl <soulvisman@gmail.com>
2021-03-04 07:48:09 +0600

    Add views && customize root styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  createStyles,
+  makeStyles,
+  Theme,
+  createMuiTheme,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import { useMachine } from "@xstate/react";
+import { gameMachine } from "./machines/gameMachine";
+import { Board } from "./views/Board";
+import { GameActorContext } from "./hooks";
+import { Profile } from "./views/Profile";
+import { Panel } from "./views/Panel";
+import { inspect } from "@xstate/inspect";
+
+if (process.env.NODE_ENV === "development") {
+  inspect({
+    iframe: false,
+  });
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    logo: {
+      color: "#3f51b5",
+      fontFamily: "'Carter One', cursive",
+      marginTop: theme.spacing(2),
+      marginBottom: 0,
+    },
+    logoHighlight: {
+      color: "#f50057",
+    },
+    content: {
+      display: "flex",
+      flexDirection: "column",
+      margin: `${theme.spacing(2)}px 0`,
+    },
+  })
+);
+
+const theme = createMuiTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 425,
+      md: 768,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+});
 
 function App() {
+  const [, , service] = useMachine(gameMachine, {
+    devTools: true,
+  });
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GameActorContext.Provider value={service}>
+      <ThemeProvider theme={theme}>
+        <div className={classes.root}>
+          <Typography variant="h3" component="h1" className={classes.logo}>
+            Memo<span className={classes.logoHighlight}>F</span>ood
+          </Typography>
+          <div className={classes.content}>
+            <Profile />
+            <Panel />
+            <Board />
+          </div>
+        </div>
+      </ThemeProvider>
+    </GameActorContext.Provider>
   );
 }
 
